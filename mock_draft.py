58df06ce4a88677502948683de949783240f11a3
wgiovani <wgiovani@terpmail.umd.edu>
2020-12-04 16:09:09 -0500

    Finishing up Draft Round

--- mock_draft.py
+++ mock_draft.py
@@ -13,6 +13,7 @@ def second(tup):
     """
     return tup[-1]
     
+#Driver: William Navigator: Sakib
 def draft_round(player_dict):
     """Sorts the players into draft rounds based on their ranks. 
     
@@ -31,16 +32,17 @@ def draft_round(player_dict):
     sorted_ranks = sorted(unsorted_ranks, key=second , reverse=True)
     
     draft_dict = dict()
-    
-    for index in range(len(sorted_ranks)):
-        if((index/8)+1 in draft_dict.keys()):
-            draft_dict[(index/8)+1].append(sorted_ranks[index][0].name)
+
+    for index in range(len(sorted_ranks)):     
+        if((index//8)+1 in draft_dict.keys()):
+            draft_dict[((index//8)+1)].append(sorted_ranks[index][0].name)
         else:
-            draft_dict[(index/8)+1] = list(sorted_ranks[index][0].name)
+            draft_dict[((index//8)+1)] = list()
+            draft_dict[((index//8)+1)].append(sorted_ranks[index][0].name)
             
-        return draft_dict
+    return draft_dict
 
-
+#Driver: Rachel Navigator: William
 def mock_draft(draft_dict):
     """Simulates an 8 person mock draft. 
     
